7b6e0c5764f5998af59dcde0da2bc8fa4fe071be
None <None>
2021-04-25 20:46:15 -0700

    added legacy_router.py

---
+++ legacy_router.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/8')
+
+    info( '*** Adding controller\n' )
+    info( '*** Add switches\n')
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
+    r1 = net.addHost('r1', cls=Node, ip='0.0.0.0')
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    info( '*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
+
+    info( '*** Add links\n')
+    net.addLink(h1, s2)
+    net.addLink(h2, s2)
+    net.addLink(s2, r1)
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s2').start([])
+
+    info( '*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
+
